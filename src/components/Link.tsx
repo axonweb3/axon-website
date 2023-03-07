18784a954154d8b0a39d833a1bc94a2ced592a42
ahonn <ahonn95@outlook.com>
2023-03-07 15:07:18 +0800

    feat: add hover animation to Link component with Framer Motion
    This commit adds a hover animation to the Link component using Framer Motion.
    The animation fades in a link-hover image on top of the link-background image when the user hovers over the link

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,33 +1,62 @@
 import React from 'react';
 import linkBackground from '../assets/img/webp/link-background.webp';
+import linkHoverBackground from '../assets/img/webp/link-hover.webp';
 import learnMoreImage from '../assets/img/webp/learn-more.webp';
 import { ILink } from '../config';
+import { motion, useAnimationControls } from 'framer-motion';
 
 export interface ILinkProps {
   item: ILink;
 }
 
+const variants = {
+  visible: {
+    opacity: 1,
+  },
+  hidden: {
+    opacity: 0,
+    transition: { ease: 'easeOut', duration: 0.5 },
+  },
+};
+
 function Link(props: ILinkProps) {
   const { item } = props;
+  const [hover, setHover] = React.useState(false);
+  const controls = useAnimationControls();
+
+  React.useEffect(() => {
+    controls.start(hover ? 'visible' : 'hidden');
+  }, [hover, controls]);
 
   return (
-    <div className="relative">
-      <img src={linkBackground} />
-      <div className="absolute top-0 left-0 h-full flex flex-col justify-between p-6">
-        <div className="flex flex-row items-center">
-          <img
-            className="h-8 md:h-6 lg:h-8 2xl:h-9 mr-2 2xl:mr-4"
-            src={item.icon}
-            loading="lazy"
-            decoding="async"
-          />
-          <span className="font-bold">{item.name}</span>
-        </div>
-        <a href={item.link}>
+    <motion.div
+      className="relative"
+      onMouseOver={() => setHover(true)}
+      onMouseOut={() => setHover(false)}
+    >
+      <a href={item.link}>
+        <img src={linkBackground} />
+        <motion.img
+          className="absolute top-0 left-0 opacity-0"
+          src={linkHoverBackground}
+          initial="hidden"
+          variants={variants}
+          animate={controls}
+        />
+        <div className="absolute top-0 left-0 h-full flex flex-col justify-between p-6">
+          <div className="flex flex-row items-center">
+            <img
+              className="h-8 md:h-6 lg:h-8 2xl:h-9 mr-2 2xl:mr-4"
+              src={item.icon}
+              loading="lazy"
+              decoding="async"
+            />
+            <span className="font-bold">{item.name}</span>
+          </div>
           <img className="h-4 2xl:h-5" src={learnMoreImage} />
-        </a>
-      </div>
-    </div>
+        </div>
+      </a>
+    </motion.div>
   );
 }
 
