c8cc23ecfb0e68a6b9212485935d106fa5511996
ahonn <ahonn95@outlook.com>
2023-03-09 22:26:10 +0800

    feat: update Vite config to remove unused image optimizer plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePluginFonts } from 'vite-plugin-fonts';
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,9 +12,6 @@ export default defineConfig({
         defer: true,
         injectTo: 'head-prepend',
       },
-    }),
-    ViteImageOptimizer({
-      /* pass your config */
     }),
   ],
 });
